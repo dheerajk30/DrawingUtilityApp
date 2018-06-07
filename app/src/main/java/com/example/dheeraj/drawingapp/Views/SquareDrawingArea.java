fd258e942e93ba252810ba262c214302c31a4986
Dheeraj Khathuria <khathuriadheeraj@gmail.com>
2018-06-07 17:23:24 +0530

    Rect draw on touch

--- app/src/main/java/com/example/dheeraj/drawingapp/Views/SquareDrawingArea.java
+++ app/src/main/java/com/example/dheeraj/drawingapp/Views/SquareDrawingArea.java
@@ -4,12 +4,16 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by dheeraj on 7/6/18.
@@ -19,6 +23,12 @@ public class SquareDrawingArea extends View {
 
     GestureDetector gestureDetector;
     Context ctx;
+    int x_centre=300,y_centre=300;
+    int length=200;
+    Boolean singletap;
+    Paint squarePaint;
+
+
     public SquareDrawingArea(Context context) {
         super(context);
         ctx=context;
@@ -44,19 +54,47 @@ public class SquareDrawingArea extends View {
     }
 
     public void init(@Nullable AttributeSet attr){
+        Toast.makeText(ctx,"init",Toast.LENGTH_SHORT).show();
         gestureDetector=new GestureDetector(ctx,new DrawGestureDetector());
+        singletap=true;
+        squarePaint=new Paint();
+        squarePaint.setColor(Color.rgb(0, 0, 0));
+        squarePaint.setStrokeWidth(10);
+        squarePaint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawColor(Color.WHITE);
+        canvas.drawRect(45f,55f,55f,45f,squarePaint);
+
+
+        if(singletap){
+            Rect rect=new Rect();
+            float left=x_centre-(float) (length/2);
+            float right=x_centre+(float) (length/2);
+
+            float top=y_centre-(float) (length/2);
+            float bottom=y_centre+(float)(length/2);
+
+            Log.d("main", "onDraw: left"+left+"right"+right+"top"+top+"bottom"+bottom);
+            canvas.drawRect(left,top,right,bottom,squarePaint);
+
+        }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        gestureDetector.onTouchEvent(event);
+
+        singletap=true;
+        x_centre=(int) event.getX();
+        y_centre=(int) event.getY();
+
+        Toast.makeText(ctx,singletap+"single tap at"+x_centre+" and"+y_centre,Toast.LENGTH_SHORT).show();
+
+        postInvalidate();
         return true;
+
     }
 
 
@@ -68,6 +106,12 @@ public class SquareDrawingArea extends View {
 
         @Override
         public boolean onSingleTapConfirmed(MotionEvent e) {
+
+            return true;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
             return true;
         }
     }
